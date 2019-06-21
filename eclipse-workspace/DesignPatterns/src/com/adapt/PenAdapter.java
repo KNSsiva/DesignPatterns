0533870d294b71fd2ba5907a2b112f8b047d3666
Sivakumar Nageswaran <sivakumar.ksnp@gmail.com>
2019-06-21 14:09:14 +0530

    Adapter Design Pattern

---
+++ eclipse-workspace/DesignPatterns/src/com/adapt/PenAdapter.java
@@ -0,0 +1,16 @@
+package com.adapt;
+
+import com.designerPen.PilotPen;
+
+public class PenAdapter implements Pen {
+	
+	PilotPen pp=new PilotPen();
+	
+	@Override
+	public void write(String str) {
+
+		pp.mark(str);
+
+	}
+
+}
